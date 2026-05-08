040c7d5f8e0d02d16ecf059de417552f5c33e146
juanjosetorres861-pixel <juanjosetorres861@gmail.com>
2026-05-07 23:13:36 -0500

    Add LocalComercial class extending Inmueble

---
+++ LocalComercial.java
@@ -0,0 +1,8 @@
+public class LocalComercial extends Inmueble {
+    private boolean viaPrincipal;
+    private int numVitrinas;
+
+    public LocalComercial(String id, String dir, double precio, double area) {
+        super(id, dir, precio, area);
+    }
+}
