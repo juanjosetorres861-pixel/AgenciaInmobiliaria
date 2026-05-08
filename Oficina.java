3f2da99a0f940574535ee2e4a0a108209a9ec685
juanjosetorres861-pixel <juanjosetorres861@gmail.com>
2026-05-07 23:15:54 -0500

    Create Oficina class extending Inmueble

---
+++ Oficina.java
@@ -0,0 +1,8 @@
+public class Oficina extends Inmueble {
+    private int numModulos;
+    private boolean tieneSalaJuntas;
+
+    public Oficina(String id, String dir, double precio, double area) {
+        super(id, dir, precio, area);
+    }
+}
