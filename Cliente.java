4486a1a3d5cb4c0a41555a0fa86b15d0e4f158ab
juanjosetorres861-pixel <juanjosetorres861@gmail.com>
2026-05-07 23:17:20 -0500

    Add Cliente class with constructor and fields

---
+++ Cliente.java
@@ -0,0 +1,11 @@
+public class Cliente {
+    private String nombre;
+    private String telefono;
+    private double presupuestoMax;
+
+    public Cliente(String nombre, String tel, double presupuesto) {
+        this.nombre = nombre;
+        this.telefono = tel;
+        this.presupuestoMax = presupuesto;
+    }
+}
